97f8d57dd8355165ce2de8e2e4dec24439d96a19
Luis R. Gonzales <luis@kerb.ai>
2019-11-11 08:30:40 -0800

    Incorporate train and val metrics; wrap step in @tf.function

--- src/train.py
+++ src/train.py
@@ -4,7 +4,6 @@ from glob import glob
 from time import time
 from datetime import timezone, datetime
 
-import numpy as np
 import tensorflow as tf
 
 from model import get_model
@@ -23,7 +22,7 @@ tf.random.set_seed(0)
 # CLI
 PARSER = argparse.ArgumentParser(description='CLI for training pipeline')
 PARSER.add_argument('--batch_size', type=int, default=32, help='Batch size per step')
-PARSER.add_argument('--epochs', type=int, default=100, help='Number of epochs')
+PARSER.add_argument('--epochs', type=int, default=120, help='Number of epochs')
 PARSER.add_argument('--learning_rate', type=float, default=1e-3, help='Initial learning rate')
 PARSER.add_argument('--optimizer', type=str, default='adam', help='Either Adam or SGD (case-insensitive)')
 PARSER.add_argument('--checkpt_freq', type=int, default=500, help='Freq of checkpt and validation')
@@ -59,9 +58,8 @@ train_ds = train_ds.map(tf_parse_filename, num_parallel_calls=AUTOTUNE)
 train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
 
 val_ds = tf.data.Dataset.list_files(VAL_FILES)
-val_ds = val_ds.repeat().batch(BATCH_SIZE, drop_remainder=True)
+val_ds = val_ds.batch(BATCH_SIZE, drop_remainder=True)
 val_ds = val_ds.map(tf_parse_filename, num_parallel_calls=AUTOTUNE)
-val_ds = iter(val_ds)
 print('Done!')
 
 
@@ -99,58 +97,96 @@ if OPTIMIZER.lower() == 'sgd':
     optimizer = tf.keras.optimizers.SGD(learning_rate=exp_decay_obj.get_next)
 elif OPTIMIZER.lower() == 'adam':
     optimizer = tf.keras.optimizers.Adam(learning_rate=exp_decay_obj.get_next)
-loss_fxn = tf.keras.losses.BinaryCrossentropy(from_logits=True) # sigmoid_cross_entropy
+loss_fxn = tf.keras.losses.BinaryCrossentropy(from_logits=True) # uses sigmoid_cross_entropy
+
+
+# Instantiate metric objects
+train_acc = tf.keras.metrics.CategoricalAccuracy()
+val_acc = tf.keras.metrics.CategoricalAccuracy()
+train_prec = tf.keras.metrics.Precision()
+train_recall = tf.keras.metrics.Recall()
+val_prec = tf.keras.metrics.Precision()
+val_recall = tf.keras.metrics.Recall()
 
 
 # Training
 print('Training...')
 print('Steps per epoch =', len(TRAIN_FILES) // BATCH_SIZE)
 print('Total steps =', (len(TRAIN_FILES) // BATCH_SIZE) * EPOCHS)
-step = 0
-for epoch in range(EPOCHS):
-    print('Epoch =', epoch)
-    for x_train, y_train in train_ds:
-        tic = time()
 
+@tf.function
+def train_step(inputs, labels, training):
+    if training:
         # Forward pass with gradient tape and loss calc
         with tf.GradientTape() as tape:
-            logits = model(x_train, training=True)
-            loss = loss_fxn(y_train, logits) + sum(model.losses)
+            logits = model(inputs, training=True)
+            loss = loss_fxn(labels, logits) + sum(model.losses)
 
         # Obtain gradients of trainable vars w.r.t. loss and perform gradient descent
         gradients = tape.gradient(loss, model.trainable_weights)
         optimizer.apply_gradients(zip(gradients, model.trainable_weights))
 
+    else:
+        logits = model(inputs, training=False)
+        loss = loss_fxn(labels, logits) + sum(model.losses)
+
+    return logits, loss, model.losses[0]
+
+step = 0
+for epoch in range(EPOCHS):
+    print('\nEpoch', epoch)
+
+    # Reset metrics
+    train_acc.reset_states()
+    val_acc.reset_states()
+    train_prec.reset_states()
+    train_recall.reset_states()
+    val_prec.reset_states()
+    val_recall.reset_states()
+
+    # Train on batches
+    for x_train, y_train in train_ds:
+        tic = time()
+
+        train_logits, train_loss, mat_reg_loss = train_step(x_train, y_train, training=True)
+
+        train_probs = tf.math.sigmoid(train_logits)
+        train_acc.update_state(y_train, train_probs)
+
+        max_idxs = tf.math.argmax(train_probs, axis=1)
+        train_one_hot = tf.one_hot(max_idxs, depth=40, dtype=tf.float32)
+        train_prec.update_state(y_train, train_one_hot)
+        train_recall.update_state(y_train, train_one_hot)
+
         if WANDB:
-            wandb.log({'learning_rate': exp_decay_obj.peek(),
-                       'training_loss': loss.numpy(),
-                       'time_per_step': time() - tic,
-                       'mat_reg_loss': model.losses[0].numpy()}, step=step)
-
-        if step % CHECKPT_FREQ == 0:
-            # np.save('pt_cloud-' + str(step), x_train[0, :, :])
-            # np.save('logits-' + str(step), logits[0, :])
-
-            # test_pt_cloud = np.load('ModelNet40/airplane/test/airplane_0627.npy')
-            # test_pt_cloud = np.expand_dims(test_pt_cloud, axis=0)
-            # print('test_pt_cloud:', test_pt_cloud.shape)
-            # test_inf = model(test_pt_cloud, training=False)
-            # def sigmoid(x):
-            #     return 1.0 / (1.0 + np.exp(-x))
-            # np.save('test_inf_sigmoid-' + str(step), sigmoid(test_inf))
-            # np.save('test_inf-' + str(step), test_inf)
-
-            print('checkpoint at step', step)
-            model.save('model/checkpoints/' + INIT_TIMESTAMP + '/iter-' + str(step), save_format='tf')
-
-            x_val, y_val = next(val_ds)
-            val_logits = model(x_val, training=False)
-            val_loss = loss_fxn(y_val, val_logits)
-            val_loss += sum(model.losses)
-
-            if WANDB:
-                wandb.log({'val_loss': val_loss.numpy(),
-                           'mat_reg_val_loss': model.losses[0].numpy()}, step=step)
+            wandb.log({'time_per_step': time() - tic,
+                       'learning_rate': exp_decay_obj.peek(),
+                       'training_loss': train_loss.numpy(),
+                       'mat_reg_loss': mat_reg_loss.numpy()}, step=step)
         step += 1
-    print('\n')
+
+    # Run validation at the end of epoch
+    for x_val, y_val in val_ds:
+        val_logits, _, _ = train_step(x_val, y_val, training=False)
+
+        val_probs = tf.math.sigmoid(val_logits)
+        val_acc.update_state(y_val, val_probs)
+
+        max_idxs = tf.math.argmax(val_probs, axis=1)
+        val_one_hot = tf.one_hot(max_idxs, depth=40, dtype=tf.float32)
+        val_prec.update_state(y_val, val_one_hot)
+        val_recall.update_state(y_val, val_one_hot)
+
+    # Save every epoch
+    print('model.save() at step', step)
+    model.save('model/checkpoints/' + INIT_TIMESTAMP + '/iter-' + str(step), save_format='tf')
+
+    if WANDB:
+        wandb.log({'train_accuracy': float(train_acc.result()),
+                   'train_precision': float(train_prec.result()),
+                   'train_recall': float(train_recall.result()),
+                   'val_accuracy': float(val_acc.result()),
+                   'val_precision': float(val_prec.result()),
+                   'val_recall': float(val_recall.result())}, step=step)
+
 print('Done training!')
